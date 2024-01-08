ac4370da61b4303f05032b73f99978c681c3df2d
deintot321 <63596225+deintot321@users.noreply.github.com>
2024-01-08 14:41:37 +0100

    done, hopefully done

--- MultiBlockDownloader.py
+++ MultiBlockDownloader.py
@@ -1,31 +1,41 @@
-import socket
 import os
 from datetime import datetime
 from urllib.parse import urlparse
-
-def download_file_in_blocks(url, block_size=16 * 1024 * 1024):
+import sys
+import socket
+def download_file_in_blocks(url, block_size=1024*1024):
     parsed_url = urlparse(url)
-    hostname = parsed_url.hostname
-    port = parsed_url.port or 80
-    path = parsed_url.path or '/'
-    file_name = path.split("/")[-1] or str(datetime.now().timestamp()) + ".bin"
+    file_name = parsed_url.path.split("/")[-1] or str(datetime.now().timestamp()) + ".bin"
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((hostname, port))
+    host = parsed_url.netloc
+    path = parsed_url.path if parsed_url.path else '/'
 
-        request = f"GET {path} HTTP/1.1\r\nHost: {hostname}\r\n\r\n"
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, 80))
+        request = f"GET {path} HTTP/1.1\r\nHost: {host}\r\n\r\n"
         s.sendall(request.encode())
-
-        received_data = b''
+        response = b''
         while True:
-            chunk = s.recv(block_size)
-            if not chunk:
+            data = s.recv(block_size)
+            if not data:
                 break
-            received_data += chunk
-
+            response += data
+        content_start = response.find(b'\r\n\r\n') + 4
         with open(file_name, 'wb') as file:
-            file.write(received_data)
-        
-        print(f"File '{file_name}' downloaded in blocks of {block_size} bytes.")
+            file.write(response[content_start:])
+            print(f"File '{file_name}' downloaded successfully.")
 
-#python3 MultiBlockDownloader.py http://fsn.icmp.hetzner.com/1GB.bin 8*1024*1024
+    except Exception as e:
+        print(f"Failed to download file from {url}: {e}")
+    finally:
+        if s:
+            s.close()
+
+if len(sys.argv) > 2:
+    download_file_in_blocks(sys.argv[1], int(sys.argv[2]))
+else:
+    download_file_in_blocks(sys.argv[1])
+
+#python3 MultiBlockDownloader.py http://fsn.icmp.hetzner.com/1GB.bin 8388608
+#python3 MultiBlockDownloader.py http://www.zoomify.com/assets/thumbnails/thmbExpressLg.jpg 8388608
--- OneBlockDownloader.py
+++ OneBlockDownloader.py
@@ -1,22 +1,45 @@
-import requests
 import os
 from datetime import datetime
 from urllib.parse import urlparse
+import sys
+import socket
 
 def download_file(url):
     parsed_url = urlparse(url)
     file_name = parsed_url.path.split("/")[-1] or str(datetime.now().timestamp()) + ".bin"
-    
-    headers = {"Host": parsed_url.netloc}
-    response = requests.get(url, headers=headers)
-    
-    if response.status_code == 200:
-        with open(file_name, 'wb') as file:
-            file.write(response.content)
-            print(f"File '{file_name}' downloaded successfully.")
-    else:
-        print(f"Failed to download file from {url}")
 
+    host = parsed_url.netloc
+    path = parsed_url.path if parsed_url.path else '/'
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, 80))
+        request = f"GET {path} HTTP/1.1\r\nHost: {host}\r\n\r\n"
+        s.sendall(request.encode())
+        response = b''
+        while True:
+            data = s.recv(1024)
+            if not data:
+                break
+            response += data
+        content_start = response.find(b'\r\n\r\n') + 4
+        with open(file_name, 'wb') as file:
+            file.write(response[content_start:])
+            print(f"File '{file_name}' downloaded successfully.")
+
+    except Exception as e:
+        print(f"Failed to download file from {url}: {e}")
+
+    finally:
+        if s:
+            s.close()
+
+if len(sys.argv) > 1:
+    download_file(sys.argv[1])
+else:
+    print("Please provide a valid URL as an argument.")
+
+download_file(sys.argv[1])
 
 #python3 OneBlockDownloader.py http://fsn.icmp.hetzner.com/1GB.bin
 #python3 OneBlockDownloader.py http://www.zoomify.com/assets/thumbnails/thmbExpressLg.jpg
